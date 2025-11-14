adce946d9f1257eda5848cb5ae9923d87086f90f
rexchoppers <rexchoppers@gmail.com>
2025-11-14 20:47:04 +0000

    REMOVED Old layout (Not required for this me thinks)

--- models/configuration.py
+++
@@ -1,9 +0,0 @@
-from beanie import Document
-from typing import Union, List
-
-class Configuration(Document):
-    field: str
-    value: Union[str, List[str]]
-
-    class Settings:
-        name = "configuration"
--- requests/update_configuration_request.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-from typing import Literal, Union, List
-
-class UpdateConfigurationRequest(BaseModel):
-    field: Literal["name", "description", "genres"]
-    value: Union[str, List[str]]
